7d0156c5e2d2bb2dc370ed2bab689e1bab12ffaa
bhagya89 <151821662+bhagya89@users.noreply.github.com>
2023-11-23 13:33:30 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,13 @@
+
+function changeClass() {
+    document.getElementById("mainDiv").style.backgroundColor = "red";
+  }
+  function removeClass() {
+    document.getElementById("mainDiv").style.backgroundColor = "blue";
+  }
+  function modifyClass() {
+    document.getElementById("mainDiv").style.backgroundColor = "pink";
+  }
+  function correctClass() {
+    document.getElementById("mainDiv").style.backgroundColor = "green";
+  }
