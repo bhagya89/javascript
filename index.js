58c481d4402cda1b5d87c24d2da5d94cc8d496b4
bhagya89 <bhagyashree.thk@gmail.com>
2023-11-23 14:42:35 +0530

    ok

---
+++ index.js
@@ -0,0 +1,29 @@
+function changeClass(){
+    document.getElementById('myDiv')
+    .className="pink";
+     let  button_class =document.getElementById('myDiv').className;
+     document.getElementById('myDiv').innerHTML= button_class;
+}
+function removeClass(){
+    document.getElementById('myDiv2')
+    .className="green";
+    let button_class=document.getElementById('myDiv2').className;
+    document.getElementById('myDiv2')
+    .innerHTML=button_class;
+}
+function modifyClass(){
+    document.getElementById('myDiv3')
+    .className="blue";
+    let button_class=document.getElementById('myDiv3').className;
+    document.getElementById('myDiv3')
+    .innerHTML= button_class;
+}
+
+function correctClass(){
+        document.getElementById('myDiv4')
+        .className="red";
+        let button_class=document.getElementById('myDiv4').className;
+        document.getElementById('myDiv4')
+        .innerHTML=button_class;
+    }
+
